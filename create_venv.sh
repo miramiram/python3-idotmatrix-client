d8efb01c7f887dd7516bc2c3c992c943de95ca07
Mira <199418076+miramiram@users.noreply.github.com>
2025-03-21 03:41:51 +0100

    Update create_venv.sh

--- create_venv.sh
+++ create_venv.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-./run_in_venv.sh -m pip install "$IDO_DIR/"
+IDO_DIR="$(dirname "$0")"
+. "$IDO_DIR"/find_cmds.sh
+$PYTHON_CMD -m venv "$IDO_DIR/venv"
+"$IDO_DIR"/run_in_venv.sh -m pip install "$IDO_DIR/"
